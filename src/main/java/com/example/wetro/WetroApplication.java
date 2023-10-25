05560595dd4c949ee55734461c90d377e00b8f35
hyunseong <rlagustjd2684@gmail.com>
2023-10-25 14:51:41 +0900

    first commit

---
+++ src/main/java/com/example/wetro/WetroApplication.java
@@ -0,0 +1,13 @@
+package com.example.wetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WetroApplication.class, args);
+    }
+
+}
