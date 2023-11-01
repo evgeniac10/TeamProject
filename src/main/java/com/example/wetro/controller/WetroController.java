0ea55b3e01f8eb3b204b18306260f691e1e7bec6
hyunseong <rlagustjd2684@gmail.com>
2023-11-02 00:06:00 +0900

    Controller 생성자 의존성 주입

--- src/main/java/com/example/wetro/controller/WetroController.java
+++ src/main/java/com/example/wetro/controller/WetroController.java
@@ -2,8 +2,10 @@ package com.example.wetro.controller;
 
 import com.example.wetro.dto.Station;
 import com.example.wetro.service.WetroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WetroController {
 
     private WetroService wetroService;
+    @Autowired
+    public WetroController(WetroService wetroService) {
+        this.wetroService = wetroService;
+    }
+
 
     @GetMapping("/map")
     public String mapPage() {
@@ -30,16 +37,16 @@ public class WetroController {
     }
 
     @PostMapping("/main")
-    public String mainInfo(Station st){
-
-
-        wetroService.searchFromStation(st.getFrom());//입력받은 출발역
-        wetroService.searchToStation(st.getTo());//입력받은 도착역
-        //입력받은 출발역,도착역이 제대로 입력을 했다면
-        //입력이 제대로 안됐다면 예외처리해주기
-        wetroService.isStation();//
-//        System.out.println("출발역 = " + station.getFrom()+ "  도착역 = "+station.getTo());
-
+    public String mainInfo(@ModelAttribute("st") Station st){
+        System.out.println("출발역 = " + st.getFrom()+ "  도착역 = "+st.getTo());
+        if (wetroService != null) {
+            wetroService.searchFromStation(st.getFrom());//입력받은 출발역
+            wetroService.searchToStation(st.getTo());//입력받은 도착역
+            wetroService.isStation();//
+        } else {
+            // wetroService가 null인 경우 예외 처리 또는 로깅 등을 수행
+            // 예: logger.error("wetroService is null");
+        }
         return "redirect:/wetro/main";
     }
 
