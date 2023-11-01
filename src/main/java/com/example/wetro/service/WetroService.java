ba6ad4f87fccca5cabcb69af7b1052be1de11393
hyunseong <rlagustjd2684@gmail.com>
2023-11-01 21:35:02 +0900

    서비스 계층 만들기

---
+++ src/main/java/com/example/wetro/service/WetroService.java
@@ -0,0 +1,19 @@
+package com.example.wetro.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class WetroService {
+
+
+    public void searchFromStation(String from) {
+    }
+
+    public void searchToStation(String to) {
+    }
+
+    public void isStation() {
+    }
+}
