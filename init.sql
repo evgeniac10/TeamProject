e933c4b9c05385923d5c8da4bb3194677e05bcde
hyunseong <rlagustjd2684@gmail.com>
2023-11-23 01:19:45 +0900

    더미데이터 추가
    user 로그인을 확인하기 위한 더미데이터 추가하는 sql 작성

--- init.sql
+++ init.sql
@@ -6,3 +6,11 @@ CREATE TABLE user (
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
+USE wetro;
+
+INSERT INTO user (id, userid, password, email) VALUES
+    (1, 'user1', '1234', 'user1@naver.com'),
+    (2, 'user2', '1234', 'user2@naver.com'),
+    (3, 'user3', '1234', 'user3@naver.com'),
+    (4, 'user4', '1234', 'user4@naver.com'),
+    (5, 'user5', '1234', 'user5@naver.com');
