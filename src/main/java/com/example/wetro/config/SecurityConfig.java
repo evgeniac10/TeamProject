6d5b626133e5e242728bb39f9fea864861fcc9ee
hyunseong <rlagustjd2684@gmail.com>
2023-11-27 01:05:48 +0900

    즐겨찾기 페이지 접근 막기

--- src/main/java/com/example/wetro/config/SecurityConfig.java
+++ src/main/java/com/example/wetro/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.wetro.config;
 
 import com.example.wetro.user.security.JwtConfigurer;
+import com.example.wetro.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,12 +14,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider tokenProvider;
+    private final UserService userService;
+    @Value("${jwt.secret}")
+    private String secretKey;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -31,7 +37,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // 특정 URL은 인증을 거치지 않도록 설정
         http.authorizeRequests()
-                    .antMatchers("/wetro/**").permitAll()
+                    .antMatchers("/wetro/main","/wetro/login","/wetro/route"
+                                ,"/wetro/map","/wetro/join").permitAll()
                     .antMatchers("/css/**").permitAll()
                     .antMatchers("/img/**").permitAll()
                     .antMatchers("/js/**").permitAll()
@@ -41,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션 사용 안함
                 .and()
                     .apply(new JwtConfigurer(tokenProvider)); // JwtConfigurer를 적용하여 JWT 사용 설정
+//                .addFilterBefore(new JwtFilter(userService,secretKey), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
