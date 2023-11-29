b3cf3bf6b860f7841d1c85fa5a311542fd6bf4d8
hyunseong <rlagustjd2684@gmail.com>
2023-11-29 20:36:25 +0900

    routedetail페이지 보안 해제

--- src/main/java/com/example/wetro/config/SecurityConfig.java
+++ src/main/java/com/example/wetro/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
 
                     .authorizeRequests()
-                    .antMatchers("/wetro/main","/wetro/login","/wetro/route"
+                    .antMatchers("/wetro/main","/wetro/login","/wetro/route","/wetro/routedetail"
                                 ,"/wetro/map","/wetro/join/**","/error").permitAll()
                     .anyRequest().authenticated()//다른 모든 요청에 대한 인증을 요구합니다.
 
