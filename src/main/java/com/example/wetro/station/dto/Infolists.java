b25dd40c60ba78b17129de4e635f504fe9822dd3
hyunseong <rlagustjd2684@gmail.com>
2023-11-30 11:34:04 +0900

    main.html

---
+++ src/main/java/com/example/wetro/station/dto/Infolists.java
@@ -0,0 +1,10 @@
+package com.example.wetro.station.dto;
+
+import lombok.Data;
+
+@Data
+public class Infolists {
+    int time;
+    int cost;
+    String transfer;
+}
---
+++ src/main/java/com/example/wetro/station/dto/StationDto.java
@@ -0,0 +1,13 @@
+package com.example.wetro.station.dto;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class StationDto {
+
+    String from;
+    String to;
+}
