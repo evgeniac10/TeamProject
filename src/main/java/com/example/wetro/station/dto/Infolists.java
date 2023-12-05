909330cdae1bdad4e7409c14b916655dab1fbd77
hyunseong <rlagustjd2684@gmail.com>
2023-12-05 22:11:54 +0900

    환승역 추가 수정

--- src/main/java/com/example/wetro/station/dto/Infolists.java
+++ src/main/java/com/example/wetro/station/dto/Infolists.java
@@ -3,10 +3,13 @@ package com.example.wetro.station.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class Infolists {
     Integer time;
     Integer cost;
     Integer transfer;
+    List<?> transStation;
 }
